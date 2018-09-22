71a67a9861202e18e7655669aad2d66978fd5f6a
LuanTalles <luangoias@hotmail.com>
2018-09-22 01:55:27 -0300

    Adding controller to Comment and some lil changes

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::CommentsController < Api::V1::ApiController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  # GET /api/v1/comments
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+
+  # GET /api/v1/comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /api/v1/comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/comments/1
+  def destroy
+    @comment.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.require(:comment).permit(:text, :user_id, :event_id)
+    end
+end
--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::EventsController < ApplicationController
+class Api::V1::EventsController < Api::V1::ApiController
   before_action :set_event, only: [:show, :update, :destroy]
 
   # GET /api/v1/events
