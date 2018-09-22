44b7b79acb4f5e1ad65c35ee54cd20a87e23876a
LuanTalles <luangoias@hotmail.com>
2018-09-22 00:55:51 -0300

    Adding unique constraint to User email and some User model validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_000951) do
+ActiveRecord::Schema.define(version: 2018_09_22_034715) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_09_22_000951) do
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
