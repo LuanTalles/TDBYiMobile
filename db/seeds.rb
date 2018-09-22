021a9d892f02911e6ef109d662fa55eeea1faec2
LuanTalles <luangoias@hotmail.com>
2018-09-21 21:19:57 -0300

    Populating User with seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+  if Rails.env.development?
+    [User].each(&:delete_all)
+
+    spinner = TTY::Spinner.new("[:spinner] Populate User ...")
+    spinner.auto_spin
+    sleep(1)
+
+    5.times do
+      User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+      )
+    end
+
+    spinner.success("")
+
+  else
+    puts "Você não está em ambiente de desenvolvimento!"
+  end
